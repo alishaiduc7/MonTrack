b7746080ba0ac24afaa07e9ca21b9dced2f6557f
Alis Haiduc <alis.haiduc7773@gmail.com>
2022-09-23 15:10:27 +0300

    UI changes for the transaction list

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -11,42 +11,61 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: transactions.map((transaction) {
-        return Card(
-          child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  margin:
-                      const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                  padding: const EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                    color: Colors.green.shade400,
-                    border: Border.all(color: Colors.green, width: 2),
-                  ),
-                  child: Text(
-                    transaction.price.toString() + ' lei',
-                    style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 15,
-                        color: Colors.white),
-                  ),
-                ),
-                Column(
-                  children: [
-                    Text(
-                      transaction.title,
-                      style: const TextStyle(
-                          fontWeight: FontWeight.bold, fontSize: 15),
-                    ),
-                    Text(DateFormat.yMMMd().format(transaction.date!),
-                        style: const TextStyle(color: Colors.grey)),
-                  ],
-                )
-              ]),
-        );
-      }).toList(),
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+            margin: const EdgeInsets.all(10),
+            child: Text(
+              'Transaction history',
+              style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.green.shade800,
+                  fontWeight: FontWeight.bold),
+            )),
+        Container(
+          height: 300,
+          child: SingleChildScrollView(
+            child: Column(
+              children: transactions.map((transaction) {
+                return Card(
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.symmetric(
+                              vertical: 10, horizontal: 15),
+                          padding: const EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            color: Colors.green.shade400,
+                            border: Border.all(color: Colors.green, width: 2),
+                          ),
+                          child: Text(
+                            transaction.price.toString() + ' lei',
+                            style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 15,
+                                color: Colors.white),
+                          ),
+                        ),
+                        Column(
+                          children: [
+                            Text(
+                              transaction.title,
+                              style: const TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 15),
+                            ),
+                            Text(DateFormat.yMMMd().format(transaction.date!),
+                                style: const TextStyle(color: Colors.grey)),
+                          ],
+                        )
+                      ]),
+                );
+              }).toList(),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
